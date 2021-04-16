e388bec5ebc0b7f18ecc3f32eece4f6ebf947f02
Andrey Mikhaylin <cenobith@gmail.com>
2021-04-16 11:00:30 +0300

    Initial Commit

---
+++ People/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  People
+//
+//  Created by Andrey Mikhaylin on 16.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Andrey Mikhaylin on 16.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
